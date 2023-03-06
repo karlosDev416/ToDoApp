d5862c11e72ccce0c7e19024eb72fa1b6b5de218
Karlos Aguirre <karlos.dev416@gmail.com>
2023-03-06 15:02:11 -0600

    Add UI Testing

--- TodoAppEndToEndTests/TodoAppEndToEndTests.swift
+++ TodoAppEndToEndTests/TodoAppEndToEndTests.swift
@@ -18,6 +18,10 @@ class when_app_is_launched: XCTestCase {
         let taskList = app.tables["taskList"]
         XCTAssertEqual(0, taskList.cells.count)
     }
+    
+    override func tearDown() {
+        Springboard.deleteApp()
+    }
 }
 
 class when_user_saves_a_new_task: XCTestCase {
@@ -36,8 +40,103 @@ class when_user_saves_a_new_task: XCTestCase {
         saveTaskButton.tap()
         
         let taskList = app.tables["taskList"]
-        XCTAssertEqual(1, taskList.cells.count)
-        
+        if taskList.waitForExistence(timeout: 1.0) {
+            XCTAssertEqual(1, taskList.cells.count)
+        }
     }
     
+    func test_display_error_message_for_duplicate_title_tasks() {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        let titleTextField = app.textFields["titleTextField"]
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        
+        let saveTaskButton = app.buttons["saveTaskButton"]
+        saveTaskButton.tap()
+        
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        
+        saveTaskButton.tap()
+        
+        let taskList = app.tables["taskList"]
+        if taskList.waitForExistence(timeout: 1.0) {
+            XCTAssertEqual(1, taskList.cells.count)
+            
+            let messageText = app.staticTexts["messageText"]
+            XCTAssertEqual(messageText.label, "Task is already added")
+        }
+    }
+    
+    override func tearDown() {
+        Springboard.deleteApp()
+    }
+    
+}
+
+class when_user_deletes_a_new_task: XCTestCase {
+    
+    private var app:XCUIApplication!
+    
+    override func setUp() {
+        app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        let titleTextField = app.textFields["titleTextField"]
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        
+        let saveTaskButton = app.buttons["saveTaskButton"]
+        saveTaskButton.tap()
+    }
+    
+    func test_should_delete_task_successfully() {
+        let cell = app.tables["taskList"].cells["Mow the lawn, Medium"]
+        if cell.waitForExistence(timeout: 1.0) {
+            cell.swipeLeft()
+            app.tables["taskList"].buttons["Delete"].tap()
+            XCTAssertFalse(cell.exists)
+        }
+    }
+    
+    override func tearDown() {
+        Springboard.deleteApp()
+    }
+}
+
+class when_users_marks_task_as_favorite: XCTestCase {
+    
+    private var app:XCUIApplication!
+    
+    override func setUp() {
+        app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        let titleTextField = app.textFields["titleTextField"]
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        
+        let saveTaskButton = app.buttons["saveTaskButton"]
+        saveTaskButton.tap()
+    }
+    
+    func test_should_displayed_updated_task_on_screen_as_favorite() {
+        
+        let cell = app.tables["taskList"].cells["Mow the lawn, Medium"]
+        if cell.waitForExistence(timeout: 1.0) {
+            cell.tap()
+            app.images["favoriteImage"].tap()
+            app.buttons["closeButton"].tap()
+            XCTAssertTrue(app.tables["taskList"].cells["Mow the lawn, Love, Medium"].exists)
+        }
+    }
+    
+    override func tearDown() {
+        Springboard.deleteApp()
+    }
 }
